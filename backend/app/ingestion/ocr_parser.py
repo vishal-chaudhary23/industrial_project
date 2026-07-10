5d976014f85284d67bcd0816322fe51e247179c6
vishal-chaudhary23 <vishalkmrchaudhary@gmail.com>
2026-07-10 19:53:51 +0530

    idk

--- backend/app/ingestion/ocr_parser.py
+++ backend/app/ingestion/ocr_parser.py
@@ -1,8 +1,20 @@
 import easyocr
 
-reader = easyocr.Reader(["en"])
+# reader = easyocr.Reader(["en"])
+reader = None
+
+def get_reader():
+    global reader
+
+    if reader is None:
+        reader = easyocr.Reader(["en"])
+
+    return reader
+
+
 
 def extract_image(image_path):
+    reader = get_reader()
 
     result = reader.readtext(image_path)
 
